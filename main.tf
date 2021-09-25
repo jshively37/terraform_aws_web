0b7b9691fff7eb287d8221cd58e38eb93c9b60b8
Jeffrey Shively <jeffrey.shively@wwt.com>
2021-09-25 17:13:02 -0400

    Working on webserver

--- main.tf
+++ main.tf
@@ -30,6 +30,25 @@ resource "aws_subnet" "public_subnet" {
   availability_zone = data.aws_availability_zones.available.names[0]
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.public_vpc.id
+
+}
+
+resource "aws_route_table" "igw_route" {
+  vpc_id = aws_vpc.public_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "private_nat" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.igw_route.id
+}
+
 data "aws_ami" "linux" {
   most_recent = true
 
