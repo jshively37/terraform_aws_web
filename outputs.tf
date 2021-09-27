5094dff6ad5ba47e9b6a696a1f75588ac0b5ba94
Jeffrey Shively <jeffrey.shively@wwt.com>
2021-09-27 08:09:40 -0400

    Adding ELB DNS to output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "instance_arn" {
   value = aws_instance.linux.*.arn
 }
 
-output "dns" {
+output "server_dns" {
   value = aws_instance.linux.*.public_dns
+}
+
+output "elb_dns" {
+  value = aws_elb.lb_web_server.dns_name
 }
